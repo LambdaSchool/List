ea20b1ff3b14fcd62fe22b8f39c6b0be7691cb1d
Audrey Welch <welch.audreyl@gmail.com>
2018-11-06 18:38:50 -0500

    Connected interface to ViewController implementation

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -5,14 +5,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
     }
 
     @IBAction func shouldAdd(_ sender: Any) {
+        //if something is entered into the textField, assign it to text. Otherwise assign nothing to text.
+        let text = textField.text ?? ""
+        
+        if !text.isEmpty { // if text is not empty...
+            // add text to the model
+            Model.shared.add(text)
+            
+            // clear the textField after each submission
+            textField.text = ""
+        }
+        // convert model's list to a string
+        let joined = Model.shared.items.joined(separator: "\n")
+        
+        // Set the label text to joined string
+        label.text = joined
     }
     
-    @IBAction func shouldReset(_ sender: Any) {
-        resetItems()
+    @IBAction func shouldReset(_ sender: Any) { // when "reset" button is tapped
+         // ... call the resetItems function (Access through the property in the class), which will clear it
+        Model.shared.resetItems()
+        
+        // Set the label text to nothing
+        label.text = ""
     }
     
     
