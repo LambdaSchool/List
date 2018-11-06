d294b08beae341d72154577e9b5a4e0532499775
Ben Hakes <bhhakes@gmail.com>
2018-11-06 16:52:50 -0600

    add list count to print button

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var list: UITextView!
+    @IBOutlet weak var printToConsoleButton: UIButton!
     @IBAction func shouldAdd(_ sender: Any) {
         let text = textField.text ?? ""
         Model.shared.add(text)
@@ -12,11 +13,20 @@ class ViewController: UIViewController {
         
         let joined = Model.shared.items.joined(separator: "\n\u{2022} ")
         list.text = "\n\u{2022} " + joined
+        
+        let itemCount = Model.shared.items.count
+        for state: UIControl.State in [.normal, .highlighted, .disabled, .selected, .focused, .application, .reserved] {
+            printToConsoleButton.setTitle(NSLocalizedString("Print " + String(itemCount) + " item(s) to Console", comment: ""), for: state)
+        }
+        
     }
     @IBAction func shouldReset(_ sender: Any) {
         Model.shared.resetItems()
         let joined = Model.shared.items.joined(separator: "\n")
         list.text = joined
+        for state: UIControl.State in [.normal] {
+            printToConsoleButton.setTitle(NSLocalizedString("Print to Console", comment: ""), for: state)
+        }
     }
     
     @IBAction func shouldPrint(_ sender: Any) {
