90e9b0d1f3636766d08b100d2c785df528f32804
Ben Hakes <bhhakes@gmail.com>
2018-11-06 16:37:48 -0600

    replace label with non-editable textview

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -4,19 +4,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var list: UITextView!
     @IBAction func shouldAdd(_ sender: Any) {
         let text = textField.text ?? ""
         Model.shared.add(text)
         textField.text = ""
         
         let joined = Model.shared.items.joined(separator: "\n\u{2022} ")
-        label.text = "\n\u{2022} " + joined
+        list.text = "\n\u{2022} " + joined
     }
     @IBAction func shouldReset(_ sender: Any) {
         Model.shared.resetItems()
         let joined = Model.shared.items.joined(separator: "\n")
-        label.text = joined
+        list.text = joined
     }
     
     @IBAction func shouldPrint(_ sender: Any) {
