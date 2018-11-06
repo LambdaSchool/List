4523222f3bd92433f3ac2e1d6fca1a28ebc404d9
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-06 15:38:00 -0800

    add a button to print items to the console log and replae the label with a textView

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -2,7 +2,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var label: UILabel!
+//    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
     @IBAction func shouldAdd(_ sender: Any) {
         let text = textField.text ?? ""
         if !text.isEmpty {
@@ -10,12 +11,24 @@ class ViewController: UIViewController {
             textField.text = ""
         }
         let joined = Model.shared.items.joined(separator: "\n")
-        label.text = joined
+//        label.text = joined
+        textView.text = joined
         }
     @IBAction func shouldReset(_ sender: Any) {
         Model.shared.resetItems()
-        label.text = ""
+//        label.text = ""
+        textView.text = ""
         }
+    @IBAction func printToConsole(_ sender: Any) {
+        let text = textField.text ?? ""
+        if !text.isEmpty {
+            Model.shared.add(text)
+            textField.text = ""
+        }
+        let joined = Model.shared.items.joined(separator: "\n")
+        print(joined)
     }
+}
+
 
 
