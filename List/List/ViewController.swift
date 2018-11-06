a4d23cf2bacc5cd6013fd801e158f9a3a19180d0
jkaunert <jkaunert@clusterfly.net>
2018-11-06 16:43:41 -0600

    replaces label with non-editable text view that is top-aligned

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var textView: UITextView!
+    
     @IBOutlet weak var label: UILabel!
     
     @IBAction func shouldAdd(_ sender: UIButton) {
@@ -12,14 +14,14 @@ class ViewController: UIViewController {
             Model.shared.add("\u{2022} \(text)")
             textField.text = ""
             let joined = Model.shared.items.joined(separator: "\n")
-            label.text = joined
+            textView.text = joined
         }
     }
     
     
     @IBAction func shouldReset(_ sender: UIButton) {
         Model.shared.resetItems()
-        label.text = ""
+        textView.text = ""
     }
 }
 
