2b76c026ef643d3aa5057e611f4a6e75a03cf00a
AustinBCole <austin.cole.chileno@gmail.com>
2018-11-06 15:02:58 -0700

    Implement initial functionality

---
+++ MVC Project/MVC Project/Model.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    private(set) var items: [String] = []
+    
+    func addItems(_ item: String) -> [String] {
+        items += [item]
+        return items
+    }
+    func  resetItems() -> [String]{
+        items.removeAll()
+        return items
+    }
+}
---
+++ MVC Project/MVC Project/ViewController.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func shouldAdd(_ sender: Any) {
+        let text = textField.text ?? ""
+        if !text.isEmpty {
+            label.text = Model.shared.addItems(text).joined(separator: "\n")
+            textField.text = ""
+        }
+    }
+    
+    @IBAction func shouldReset(_ sender: Any) {
+        label.text = Model.shared.resetItems().joined(separator: "\n")
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
