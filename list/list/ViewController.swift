4ed2b1942f184ebcd58280620ddf67dcdcb3d8e9
Stuart Sippel <stuartsippel@icloud.com>
2018-11-06 18:29:45 -0600

    Add base xcode proj

---
+++ list/list/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func shouldAdd(_ sender: Any) {
+    }
+    
+    @IBAction func shouldReset(_ sender: Any) {
+    }
+    
+}
+
+
