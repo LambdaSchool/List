0ff45794397549c69946afbd9fa9293431ffa087
Vijay Das <vijaytdas@gmail.com>
2018-11-06 18:20:32 -0600

    version with first two stretch goals

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -8,7 +8,7 @@ class ViewController: UIViewController {
     @IBAction func shouldAdd(_ sender: UITextField) {
         let text = textField.text ?? ""
         if !text.isEmpty {
-        Model.shared.add(text)
+        Model.shared.add("* \(text)")
         textField.text = ""
       }
         let joined = Model.shared.items.joined(separator: "\n")
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     @IBAction func shouldReset(_ sender: UIButton) {
         Model.shared.resetItems()
         label.text = ""
+    }
+    @IBAction func print(_ sender: UIButton) {
+        Swift.print(Model.shared.items)
     }
 }
 
