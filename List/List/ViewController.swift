a9a091d25dc816d6b44a6c43d0875e403801e8ca
Vijay Das <vijaytdas@gmail.com>
2018-11-06 19:00:42 -0600

    version with third stretch

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -3,7 +3,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var label: UILabel!
+    //    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
     
     @IBAction func shouldAdd(_ sender: UITextField) {
         let text = textField.text ?? ""
@@ -12,12 +13,12 @@ class ViewController: UIViewController {
         textField.text = ""
       }
         let joined = Model.shared.items.joined(separator: "\n")
-        label.text = joined
+        textView.text = joined
     }
         
     @IBAction func shouldReset(_ sender: UIButton) {
         Model.shared.resetItems()
-        label.text = ""
+        textView.text = ""
     }
     @IBAction func print(_ sender: UIButton) {
         Swift.print(Model.shared.items)
