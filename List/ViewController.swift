1ee1f0f6877a851088b6d1df6052c5bf5a77afe3
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-06 16:20:51 -0800

    Completed Stretch Problems - Updated List Questions

--- List/ViewController.swift
+++ List/ViewController.swift
@@ -11,26 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var label: UILabel!
-    
-    
+    @IBOutlet weak var textView: UITextView!
+    //var joined = ""
     @IBAction func shouldAdd(_ sender: Any) {
         let text = textField.text ?? ""
+        let bullet = " \u{F8FF} "
         if !text.isEmpty {
-            Model.shared.add(text)
+            Model.shared.add(bullet + text)
         }
         
         textField.text = ""
         
         for _ in Model.shared.items {
             let joined = Model.shared.items.joined(separator: "\n")
-            label.text = joined
+            textView.text = joined
         }
     }
     
     @IBAction func shoudReset(_ sender: Any) {
         Model.shared.reset()
-        label.text = ""
+        textView.text = ""
+    }
+    
+
+    @IBAction func printConsole(_ sender: Any) {
+        print(Model.shared.items)
     }
 }
 
