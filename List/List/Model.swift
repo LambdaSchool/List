b97ad03cf54ce638617b129eb6fb8004c2d87fdd
Sameera Leola <sameeraleola@gmail.com>
2018-11-06 18:37:31 -0600

    Added Questions.txt file which contains answers to challenge questions

--- List/List/Model.swift
+++ List/List/Model.swift
@@ -8,11 +8,14 @@ class Model {
         // add the item to your items list
         let bulletPoint: String = "\u{2022}"
         items.append(bulletPoint + " " + item)
-        print(items)
     }
     
     func resetItems() {
         // reset your items list here
         items = []
+    }
+    
+    func printItems() {
+        // print items to the debug console
     }
 }
