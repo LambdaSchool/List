911393f85cd1232be77a8a39f7f481d4013c0aa6
Meera Andersen <meera.andersen@gmail.com>
2018-11-13 16:00:00 -0700

    Built out the interface

---
+++ List Project/List Project/Model.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    private(set) var items: [String] = []
+    
+    func add(_ item: String) {
+       items.append(item)
+    }
+   
+    
+    func resetItems() {
+        items = []
+    }
+    
+    
+    
+    
+}
