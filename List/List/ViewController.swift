fb441fc024d904bf55c980b6ba04bd4a77fd7216
Vijay Das <vijaytdas@gmail.com>
2018-11-13 19:58:24 -0600

    add model with add function and remove function

---
+++ List/List/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Vijay Das on 11/13/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+}
+
