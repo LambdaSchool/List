d7279758fdf0672a11aee3fe8fbaed7e93f6186c
Vijay Das <vijaytdas@gmail.com>
2018-11-13 21:27:42 -0600

    add interface, add outlets and actions, add shouldAdd and shouldRemove

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
     
     
+    @IBAction func shouldAdd(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+            Model.shared.addItem(item: text)
+            self.textField.text = ""
+     }
+    
+    @IBAction func shouldReset(_ sender: Any) {
+        Model.shared.resetItems()
+        textField.text = ""
+    
+    }
     
 }
+
 
