e1ed5bf0276a0fd09ba4f316493460805b6f0f1e
Michael R.F <mrflowers22@uwalumni.com>
2019-01-08 16:05:40 -0500

    Add UIView elements on main.storyboard and create model file and model controller file.

---
+++ mylist/mylist/List.swift
@@ -0,0 +1,14 @@
+//
+//  List.swift
+//  mylist
+//
+//  Created by Michael Flowers on 1/8/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+//Using a struct because we have one value that doesn't need to be changed or updated later in this program.
+struct List {
+    var word: String
+}
---
+++ mylist/mylist/ListController.swift
@@ -0,0 +1,17 @@
+//
+//  ListController.swift
+//  mylist
+//
+//  Created by Michael Flowers on 1/8/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class ListController {
+    
+    //Create an array to hold the lists
+    var lists = [List]()
+    
+    
+}
