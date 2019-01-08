a1f8edd4990f7974f58421effd0f6834e120304b
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-01-08 16:26:51 -0600

    created a textfield that will add items to an array every time the add button is pressed

---
+++ ItemList/ItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemModel.swift
+//  ItemList
+//
+//  Created by Diante Lewis-Jolley on 1/8/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    var name: String
+}
