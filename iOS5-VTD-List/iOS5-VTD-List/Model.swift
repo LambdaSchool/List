ff248398f1342aebdbaa03dc23de706258cc97d5
Vijay Das <vijaytdas@gmail.com>
2019-01-08 07:58:01 -0600

    Added model for the list items

---
+++ iOS5-VTD-List/iOS5-VTD-List/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  iOS5-VTD-List
+//
+//  Created by Vijay Das on 1/8/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    private(set) var items: [String] = []
+}
