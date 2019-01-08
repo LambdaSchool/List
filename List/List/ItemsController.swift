cb6e32d2da6653dc2f146dd75b4e124ecd6380ab
Sean Doyle <sdoylelambda@gmail.com>
2019-01-08 18:22:25 -0500

    Functions added to add a new item and reset item list.

--- List/List/ItemsController.swift
+++ List/List/ItemsController.swift
@@ -1,2 +1,16 @@
 import Foundation
 
+class ItemsController {
+    var items: [String] = []
+    
+    func add(_ item: String) {
+        return items.append(item)
+    }
+    
+    func resetItems() {
+        // reset your items list here
+       return items.reset
+    }
+}
+
+
