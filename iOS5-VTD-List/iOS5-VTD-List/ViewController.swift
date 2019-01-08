be74d824f7c869e5785b0079bbe3129b55dbc6ba
Vijay Das <vijaytdas@gmail.com>
2019-01-08 14:11:49 -0600

    MVP complete

--- iOS5-VTD-List/iOS5-VTD-List/ViewController.swift
+++ iOS5-VTD-List/iOS5-VTD-List/ViewController.swift
@@ -15,6 +15,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var textfield: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func shouldAdd(_ sender: UIButton) {
+        let text = textfield.text ?? ""
+        if !text.isEmpty {
+            Model.shared.addItem(text)
+            textfield.text = ""
+            }
+        let joined = Model.shared.items.joined(separator: "\n")
+        label.text = joined
+        
+    }
+    
+    @IBAction func shouldReset(_ sender: UIButton) {
+        Model.shared.resetItems()
+        textfield.text = ""
+        label.text = ""
+        
+    }
+    
+    
+    
 }
 
