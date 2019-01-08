c67a358e4ff89bbd5fa9fd44c678f7584db175dc
Mary Sabol <mary@marys-mbp>
2019-01-08 15:58:32 -0500

    Interface created with button, text field, and label

---
+++ petsList/petsList/Item.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  petsList
+//
+//  Created by Mary Kajiwara on 1/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    var name: String
+    
+}
+
+func add(_ item: String) {
+    // add the item to your items list
+}
+
+func resetItems() {
+    // reset your items list here
+}
---
+++ petsList/petsList/ItemsController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsController.swift
+//  petsList
+//
+//  Created by Mary Kajiwara on 1/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ItemsController {
+    var items: [String] = []
+}
