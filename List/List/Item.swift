3747459fa5f1f3301bf6bbe7225b8bbb82b6a737
Luis Corvalan <LRCorvalan@gmail.com>
2019-01-08 15:20:52 -0600

    Add Question list from Pages. Done Project setup.

---
+++ List/List/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  List
+//
+//  Created by Luis Corvalan on 1/8/19.
+//  Copyright © 2019 Luis Corvalan. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    var name: String
+}
+
+
+
+
+
+
---
+++ List/List/ItemsController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsController.swift
+//  List
+//
+//  Created by Luis Corvalan on 1/8/19.
+//  Copyright © 2019 Luis Corvalan. All rights reserved.
+//
+
+import Foundation
+
+
+class ItemsController {
+    var items: [String] = []
+}
+
