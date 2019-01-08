af5a44df3ca95c1cbc84fa691e37581b68d4683d
Angel <angelbuenrostro@outlook.com>
2019-01-08 15:49:49 -0500

    Add UI elements and create necessary functions for a Model class to implement a list for the app.

---
+++ ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Angel Buenrostro on 1/8/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func shouldAdd(_ sender: Any) {
+        let text = textField.text ?? ""
+        if !text.isEmpty {
+            Model.shared.add(text)
+            textField.text = ""
+            let joined = Model.shared.items.joined(separator: "\n")
+            label.text = joined
+        }
+    }
+    
+    @IBAction func shouldReset(_ sender: Any) {
+        Model.shared.resetItems()
+        label.text = ""
+    }
+    
+}
+
