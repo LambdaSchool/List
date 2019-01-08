e877e0e6c8019861a1fce066b6a2f7b0c0bbd15b
Michael R.F <mrflowers22@uwalumni.com>
2019-01-08 16:29:45 -0500

    replace textView with label.

--- mylist/mylist/View Controller/ViewController.swift
+++ mylist/mylist/View Controller/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var itemController = ItemController()
 
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var listTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        listTextView.text = nil
     }
 
-
+    @IBAction func addItem(_ sender: UIButton) {
+        guard let text = itemTextField.text, !text.isEmpty else { return }
+        itemController.items.insert(text, at: 0)
+        
+        listTextView.text = itemController.items.first
+    }
+    
+    @IBAction func resetList(_ sender: UIButton) {
+    }
+    
 }
 
